16e482300e2479b736a3f14b914117b245cfca9b
Richard Hansen <RichardHansen@genomicss-mbp.lan>
2021-01-26 12:14:07 -0500

    Made generate.py into a class, created first draft of config, updated gitingore to allow the config example

--- generate.py
+++ generate.py
@@ -1,13 +1,19 @@
-# Generates the config file used by c7n-org, which specifies all the accounts, regions, and roles we will use to deploy policies
-def generate_custodian_config():
-    pass
 
+# generates all the necessary files to deploy policies given a configuration file in the format specified in /config.yml.template
+class Generator:
+    CUSTODIAN_GENERATED_DIR = "generated/custodian/"
+    TERRAFORM_GENERATED_DIR = "generated/terraform/"
 
-# Generates the policy file that will be deployed as a resource using custodian
-def generate_custodian_policy():
-    pass
+    def __init__(self):
+        pass
+    # Generates the config file used by c7n-org, which specifies all the accounts, regions, and roles we will use to deploy policies
+    def generate_custodian_config(self):
+        pass
 
+    # Generates the policy file that will be deployed as a resource using custodian
+    def generate_custodian_policy(self):
+        pass
 
-# Generates the terraform file that will deploy the IAM roles and policies needed by custodian
-def generate_terraform():
-    pass
+    # Generates the terraform file that will deploy the IAM roles and policies needed by custodian
+    def generate_terraform(self):
+        pass
