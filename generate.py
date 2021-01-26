f7bccfacb2065f96ff8a1a1d7b54baa135c76197
Richard Hansen <RichardHansen@genomicss-mbp.lan>
2021-01-26 11:34:33 -0500

    Added quick documentation blurb

--- generate.py
+++ generate.py
@@ -1,10 +1,13 @@
+# Generates the config file used by c7n-org, which specifies all the accounts, regions, and roles we will use to deploy policies
 def generate_custodian_config():
     pass
 
 
+# Generates the policy file that will be deployed as a resource using custodian
 def generate_custodian_policy():
     pass
 
 
+# Generates the terraform file that will deploy the IAM roles and policies needed by custodian
 def generate_terraform():
     pass
