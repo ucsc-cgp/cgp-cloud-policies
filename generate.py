543ffe9de9f1e26c60d46d829d0f12496dfa9dea
Richard Hansen <RichardHansen@genomicss-mbp.lan>
2021-01-29 10:18:55 -0500

    Removed unused main (was there for testing), also used more appropriate errors for not implemented functions

--- generate.py
+++ generate.py
@@ -17,16 +17,12 @@ class Generator:
 
     # Generates the config file used by c7n-org, which specifies all the accounts, regions, and roles we will use to deploy policies
     def generate_custodian_config(self):
-        pass
+        raise NotImplementedError()
 
     # Generates the policy file that will be deployed as a resource using custodian
     def generate_custodian_policy(self):
-        pass
+        raise NotImplementedError()
 
     # Generates the terraform file that will deploy the IAM roles and policies needed by custodian
     def generate_terraform(self):
         return iam_template.terraform_iam_template(self.config)
-
-
-if __name__ == "__main__":
-    pass
