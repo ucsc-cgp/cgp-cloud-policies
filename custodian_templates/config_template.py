49caf4cf4341f633049c881dd41198dc84a9cd23
Richard Hansen <RichardHansen@genomicss-mbp.lan>
2021-01-29 11:02:45 -0500

    Added custodian organization config template

---
+++ custodian_templates/config_template.py
@@ -0,0 +1,20 @@
+from typing import Mapping
+
+
+def custodian_organizations_config_template(config: Mapping) -> Mapping:
+    dict_template = {
+        "accounts": [
+            {
+                "account_id": account["account_id"],
+                "name": account["account_name"],
+                "regions": [region for region in config["aws"]["regions"]],
+                "role": _create_role_string(account["account_id"], config["aws"]["IAM_role_name"])
+            } for account in config["aws"]["accounts"]
+        ]
+    }
+
+    return dict_template
+
+
+def _create_role_string(account_id: str, iam_role_name: str) -> str:
+    return "arn:aws:iam::" + account_id + ":role/" + iam_role_name
