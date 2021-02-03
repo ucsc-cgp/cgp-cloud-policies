4dea8eb04912db910290e42fb0336a739ad804ea
richard-hansen <richie.hansen@gmail.com>
2021-02-03 13:58:00 -0500

    Changed class Generator to ConfigGenerator to avoid any class naming conflicts

--- generate.py
+++ generate.py
@@ -4,7 +4,7 @@ from terraform_templates import iam_template
 
 
 # generates all the necessary files to deploy policies given a configuration file in the format specified in /config.yml.template
-class Generator:
+class ConfigGenerator:
     CUSTODIAN_GENERATED_DIR = "generated/custodian/"
     TERRAFORM_GENERATED_DIR = "generated/terraform/"
 
