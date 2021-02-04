e8ff4600054dd934cb8215b016aafae2e10c08a1
richard-hansen <richie.hansen@gmail.com>
2021-02-04 17:33:07 -0500

    generating custodian config and policy files

--- custodian_templates/policy_template.py
+++ custodian_templates/policy_template.py
@@ -5,7 +5,7 @@ def custodian_policy_template(config: Mapping) -> Mapping:
     dict_template = {
         "policies": [
             {
-                "name": _create_policy_name(config["aws"]["custodian_policy_prefix"], resource),
+                "name": config["aws"]["custodian_policy_prefix"] + resource,
                 "mode": {
                     "type": "config-rule"
                 },
@@ -18,8 +18,3 @@ def custodian_policy_template(config: Mapping) -> Mapping:
     }
 
     return dict_template
-
-
-def _create_policy_name(prefix: str, resource: str) -> str:
-    provider, resource_type = resource.split(".")
-    return prefix + resource_type
