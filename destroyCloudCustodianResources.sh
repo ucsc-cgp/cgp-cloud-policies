4760d228aaa22621501cb20ce7afe31dec07c3a4
richard-hansen <richie.hansen@gmail.com>
2021-03-23 15:14:51 -0400

    Weird spacing issue

--- destroyCloudCustodianResources.sh
+++ destroyCloudCustodianResources.sh
@@ -6,5 +6,5 @@ rolesArray=(${ROLES})
 for (( i=0; i<${#accountsArray[@]}; i++));
   do
     echo "Destroying CloudCustodian resources on ${accountsArray[$i]}"
-		c7n-org run-script --output-dir s3://$S3_STATE_BUCKET/$CUSTODIAN_PREFIX -c generated/custodian/generated_custodian_config.json -a ${accountsArray[$i]} "python3 utils/mugc.py --assume ${rolesArray[$i]} --present -c generated/custodian/generated_custodian_policy.json"
+    c7n-org run-script --output-dir s3://$S3_STATE_BUCKET/$CUSTODIAN_PREFIX -c generated/custodian/generated_custodian_config.json -a ${accountsArray[$i]} "python3 utils/mugc.py --assume ${rolesArray[$i]} --present -c generated/custodian/generated_custodian_policy.json"
   done
